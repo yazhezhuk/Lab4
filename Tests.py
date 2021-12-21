00e05a41b31cc82d1903fc250998326280532e88
Vitalich <zhukovets.vetal322@gmail.com>
2021-12-22 00:52:04 +0200

    lab 5 done

--- Tests.py
+++ Tests.py
@@ -1,8 +1,31 @@
 from unittest import TestCase
 from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+
+
+def waitElementTextLoading(by, value, driver):
+    element = None
+    rez = ""
+    while rez is "":
+        try:
+            rez = driver.find_element(by, value=value).text
+            break
+        except StaleElementReferenceException:
+            continue
+    return rez
+
+
+def waitElementSendKey(by, value, driver, keys):
+    element = None
+    while element is None:
+        try:
+            driver.find_element(by, value=value).send_keys(keys)
+            break
+        except StaleElementReferenceException:
+            continue
 
 
 class Tests(TestCase):
@@ -31,30 +54,28 @@ class Tests(TestCase):
 
         browser = webdriver.Chrome(ChromeDriverManager().install())
 
+        browser.implicitly_wait(10)
+
         browser.get(url)
 
-        browser.find_element(by=By.XPATH, value='/html/body/div[1]/header/div[2]/div/div/div[2]/form/input').send_keys(
-            search_text)
-        browser.find_element(by=By.XPATH, value='/html/body/div[1]/header/div[2]/div/div/div[2]/form/input').send_keys(
-            Keys.ENTER)
+        waitElementSendKey(by=By.XPATH,
+                                  value='/html/body/div[1]/header/div[2]/div/div/div[2]/form/input',
+                                  driver=browser,
+                                  keys=search_text)
+        waitElementSendKey(by=By.XPATH,
+                           value='/html/body/div[1]/header/div[2]/div/div/div[2]/form/input',
+                           driver=browser,
+                           keys=Keys.ENTER)
 
-        browser.implicitly_wait(10)
 
         selector = "#__layout > div > div.search.container > div.flex.top-xs > " \
                    "div.search-sidebar.search__sidebar.hidden-below-xl > div:nth-child(1) > div:nth-child(1) > " \
                    "div.search-sidebar-section__header.flex.middle-xs > div > b "
-        actual_category = self.retryingFindClick(selector ,browser)
+        actual_category = waitElementTextLoading(by=By.CSS_SELECTOR,
+                                             value=selector,
+                                             driver=browser)
+
+        browser.implicitly_wait(10)
 
         assert expected_category == actual_category
         browser.close()
-
-    def retryingFindClick(self, by, driver):
-        result = False
-        text = ""
-        while text == "":
-            try:
-                text = driver.find_element(By.CSS_SELECTOR,value=by).text
-                break
-            except():
-                continue
-        return text
